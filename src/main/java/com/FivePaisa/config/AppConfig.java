dc274255405fc2652d5a1caf866012cb833f81a1
Andy <developer.adeep@gmail.com>
2024-02-21 16:06:52 -0500

    Code Cleanup

--- src/main/java/com/FivePaisa/config/AppConfig.java
+++ src/main/java/com/FivePaisa/config/AppConfig.java
@@ -1,88 +1,28 @@
 package com.FivePaisa.config;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class AppConfig {
 
-	public String encryptKey = "Your encryption key";
+    public String encryptKey = "Your encryption key";
 
-	public String key = "Your user key";
+    public String key = "Your user key";
 
-	public String appVer = "1.0";
+    public String appVer = "1.0";
 
-	public String appName = "Your App Name";
+    public String appName = "Your App Name";
 
-	public String osName = "WEB";
+    public String osName = "WEB";
 
-	public String loginId = "Your client code";
+    public String clientCode = "Your client code";
 
-	public String userId = "Your userId";
+    public String userId = "Your userId";
 
-	public String password = "Your password";
-
-	public AppConfig() {
-	}
-
-	public String getEncryptKey() {
-		return encryptKey;
-	}
-
-	public void setEncryptKey(String encryptKey) {
-		this.encryptKey = encryptKey;
-	}
-
-	public String getKey() {
-		return key;
-	}
-
-	public void setKey(String key) {
-		this.key = key;
-	}
-
-	public String getAppVer() {
-		return appVer;
-	}
-
-	public void setAppVer(String appVer) {
-		this.appVer = appVer;
-	}
-
-	public String getAppName() {
-		return appName;
-	}
-
-	public void setAppName(String appName) {
-		this.appName = appName;
-	}
-
-	public String getOsName() {
-		return osName;
-	}
-
-	public void setOsName(String osName) {
-		this.osName = osName;
-	}
-
-	public String getLoginId() {
-		return loginId;
-	}
-
-	public void setLoginId(String loginId) {
-		this.loginId = loginId;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String password = "Your password";
 
 }
