f0762694ab3e159fd0b94b4f961db21dd7061fe5
Andy <developer.adeep@gmail.com>
2024-02-27 05:45:05 -0500

    BO

--- src/main/java/com/FivePaisa/service/NetworkUtils.java
+++ src/main/java/com/FivePaisa/service/NetworkUtils.java
@@ -59,6 +59,7 @@ public class NetworkUtils {
             case SQUARE_OFF -> "https://Openapi.5paisa.com/VendorsAPI/Service1.svc/SquareOffAll";
             case TRADE_BOOK -> "https://Openapi.5paisa.com/VendorsAPI/Service1.svc/V1/TradeBook";
             case PLACE_ORDER -> "https://openapi.5paisa.com/VendorsAPI/Service1.svc/V1/PlaceOrderRequest";
+            case PLACE_BRACKET_ORDER -> "https://openapi.5paisa.com/VendorsAPI/Service1.svc/V1/BracketOrderRequest";
             default -> "/";
         };
     }
